7f5768c22caee5103b8d6858942800596c5171a6
Summer <nathandouglaslunn@gmail.com>
2022-11-18 05:49:52 -0500

    set up front end image upload input to test post request to cloudinary api

--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 export default function PostHome() {
   const [todaysPost, setTodaysPost] = useState({});
+  const [imageSelected, setImageSelected] = useState({});
 
   useEffect(() => {
     axios.get('https://daylee-backend.onrender.com/posts')
@@ -12,14 +13,35 @@ export default function PostHome() {
     .catch(err => {
       console.error(err.message);
     })
-  }, [])
+  }, []);
+
+  const uploadImage = (files) => {
+    const formData = new FormData();
+    formData.append('file', imageSelected);
+    formData.append('upload_preset', 'pho9c5mj');
+
+    axios.post('https://api.cloudinary.come/v1_1/dnggclzfd/image/upload', formData)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err.message);
+    })
+  };
 
   return (
     <div>
       {todaysPost.description}
 
-      <input type='file'>
-      </input>
+      <input 
+        type='file'
+        onChange={(e) => {
+          setImageSelected(e.target.files[0])
+        }}
+      ></input>
+      <button
+        onClick={uploadImage}
+      >Upload Image</button>
     </div>
   )
 }
