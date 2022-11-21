54d0149c83e0d63e8a1d148f6e8ad73a1b85bafc
Summer <nathandouglaslunn@gmail.com>
2022-11-21 10:27:45 -0500

    start creating and styling navbar using sass

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Nav.css';
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div className='nav'>
+      <Link className='nav__link' to='/'>Home</Link>
+      <Link className='nav__link' to='/login'>Login</Link>
+      <Link className='nav__link' to='/signup'>Sign Up</Link>
+    </div>
   )
 }
--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {Image} from 'cloudinary-react';
 
 export default function PostHome({state, setState}) {
   console.log(state);
 
   return (
     <div>
-      {state.topic.title}
+      <Image 
+        cloudName='dnggclzfd'
+        publicId={state.topic.image}
+      />
+      <h2>{state.topic.title}</h2>
     </div>
 
   )
