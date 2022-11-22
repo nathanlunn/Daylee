eab9658df299913fe575e61f919dd2bfe09a9843
Summer <nathandouglaslunn@gmail.com>
2022-11-22 15:02:31 -0500

    start styling for Daylee topic page

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import '../styles/Nav.css';
+import name from '../assets/name.png';
 
 export default function Nav() {
   return (
     <div className='nav'>
-      <Link className='nav__link' to='/'>HOME</Link>
-      <Link className='nav__link' to='/login'>LOGIN</Link>
-      <Link className='nav__link' to='/signup'>SIGN UP</Link>
+      <img className='nav__name' src={name}/>
+      <div>
+        <Link className='nav__link' to='/'>HOME</Link>
+        <Link className='nav__link' to='/login'>LOGIN</Link>
+        <Link className='nav__link' to='/signup'>SIGN UP</Link>
+      </div>
     </div>
   )
 }
--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -1,17 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Image} from 'cloudinary-react';
+import '../styles/PostHome.css';
 
 export default function PostHome({state, setState}) {
   console.log(state);
 
   return (
-    <div>
+    <div className='topic'>
       <Image 
+        className='topic__image'
         cloudName='dnggclzfd'
         publicId={state.topic.image}
       />
-      <h2>{state.topic.title}</h2>
+      <h2 className='topic__title'>{state.topic.title}</h2>
     </div>
 
   )
