cf7f5a127e669fe721106b664b876536bdaf26ad
Summer <nathandouglaslunn@gmail.com>
2022-11-22 15:17:56 -0500

    start login configuration

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Login() {
+export default function Login({state, setState}) {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <h2 className='login__title'>Login:</h2>
+      <input 
+        className='login__input login__input--email'
+        value={email}
+      ></input>
+      <div className='login__passwordContainer'>
+        <input 
+          className='login__input login__input--password'
+          value={password}
+          type={showPassword ? 'text' : 'password'}
+        ></input>
+      </div>
+    </div>
   )
 }
