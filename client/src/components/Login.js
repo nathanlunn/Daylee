d4d191991f449d4ab082ddc764111052a62c6447
Summer <nathandouglaslunn@gmail.com>
2022-11-22 16:41:59 -0500

    create working axios connection when clicking submit on login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,20 @@
+import axios from 'axios';
 import React, {useState} from 'react'
 
 export default function Login({state, setState}) {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const login = () => {
+    axios.post('http://localhost:8000/users/login', [email, password])
+      .then(res => {
+
+      })
+      .catch(err => {
+        console.error(err.message);
+      })
+  }
 
   return (
     <div className='login'>
@@ -25,7 +36,18 @@ export default function Login({state, setState}) {
             setPassword(e.target.value);
           }} 
         ></input>
+        {showPassword ? (<i 
+          class="fa-solid fa-eye-slash"
+          onClick={() => {setShowPassword(false)}}
+        ></i>) : (<i
+          class="fa-solid fa-eye"
+          onClick={() => {setShowPassword(true)}}
+        ></i>)}
       </div>
+      <button
+        className='login__submit'
+        onClick={login}
+      >Login</button>
     </div>
   )
 }
--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.send('working');
 })
 
+router.post('/login', (req, res) => {
+  console.log('working');
+})
+
 module.exports = router;
