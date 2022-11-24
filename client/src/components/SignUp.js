081e024b36e4a1ed9d69f83a67d3e6690bf3a2b2
Summer <nathandouglaslunn@gmail.com>
2022-11-24 06:41:50 -0500

    create loading and error message states in SignUp component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,12 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Image} from 'cloudinary-react';
+import '../styles/SignUp.css';
 
 export default function SignUp({state, setState}) {
   const [errorMessage, setErrorMessage] = useState('none');
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className='signup'>
+      {loading && <div className='login__spinner'></div>}
+
       <h2
         className={errorMessage === 'none' ? 'signup__error hide' : 'signup__error'}
       >{errorMessage}</h2>
