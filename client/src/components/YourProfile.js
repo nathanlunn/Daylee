07dd3ee53fb16d0008b699f7345e0ccc21404f2d
Summer <nathandouglaslunn@gmail.com>
2022-11-28 06:08:30 -0500

    display profile information on profile page

--- client/src/components/YourProfile.js
+++ client/src/components/YourProfile.js
@@ -1,10 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import '../styles/YourProfile.css';
+import {Image} from 'cloudinary-react';
 
 export default function YourProfile({state, setState}) {
   return (
     <div className='profile'>
+      <div className='profile__imageContainer'>
+        <Image
+          className='profile__image'
+          cloudName='dnggclzfd'
+          publicId={state.user.image}
+        />
+      </div>
 
+      <h5 className='profile__title'>name:</h5>
+      <div className='profile__nameContainer'>
+        <i class="fa-solid fa-pen-to-square hide"></i>
+        <h2 className='profile__info'>{state.user.name}</h2>
+        <i class="fa-solid fa-pen-to-square"></i>
+      </div>
+      
+      <h5 className='profile__title'>email:</h5>
+      <h2 className='profile__info'>{state.user.email}</h2>
+
+      <h5 className='profile__title'>name:</h5>
+      <div className='profile__nameContainer'>
+        <i class="fa-solid fa-pen-to-square hide"></i>
+        <h2 className='profile__info'>{state.user.name}</h2>
+        <i class="fa-solid fa-pen-to-square"></i>
+      </div>
     </div>
   )
 }
