75935225a9264419500d4d3fc1bd30cfe0853d98
Summer <nathandouglaslunn@gmail.com>
2022-11-28 06:39:41 -0500

    start creating ternerary operator for image container whether or not the user has clicked the edit button

--- client/src/components/YourProfile.js
+++ client/src/components/YourProfile.js
@@ -6,20 +6,33 @@ export default function YourProfile({state, setState}) {
   const [changeImage, setChangeImage] = useState(false);
   const [changeName, setChangeName] = useState(false);
   const [changeBio, setChangeBio] = useState(false);
+  const [imageURL, setImageURL] = useState('');
 
   return (
     <div className='profile'>
       <div className='profile__imageContainer'>
-        <i class="fa-solid fa-pen-to-square profile__imageIcon hide"></i>
-        <Image
-          className='profile__image'
-          cloudName='dnggclzfd'
-          publicId={state.user.image}
-        />
-        <i 
-          class="fa-solid fa-pen-to-square profile__imageIcon"  
-          onClick={() => setChangeImage(true)}
-        ></i>
+        {changeImage ? (
+          <div>
+            <Image
+              className='profile__image'
+              cloudName='dnggclzfd'
+              publicId={imageURL || state.user.image}
+            />
+          </div>
+        ) : (
+          <>
+            <i class="fa-solid fa-pen-to-square profile__imageIcon hide"></i>
+            <Image
+              className='profile__image'
+              cloudName='dnggclzfd'
+              publicId={state.user.image}
+            />
+            <i 
+              class="fa-solid fa-pen-to-square profile__imageIcon"  
+              onClick={() => setChangeImage(true)}
+            ></i>
+          </>
+        )}
       </div>
 
       <h5 className='profile__title'>name:</h5>
