ed2c90bc695912044aa0a6bbf401662246097b17
Summer <nathandouglaslunn@gmail.com>
2022-11-28 06:33:12 -0500

    create change input states for image, name, and bio that changes from false to true whenever the edit icon is clicked for that attribute

--- client/src/components/YourProfile.js
+++ client/src/components/YourProfile.js
@@ -3,6 +3,10 @@ import '../styles/YourProfile.css';
 import {Image} from 'cloudinary-react';
 
 export default function YourProfile({state, setState}) {
+  const [changeImage, setChangeImage] = useState(false);
+  const [changeName, setChangeName] = useState(false);
+  const [changeBio, setChangeBio] = useState(false);
+
   return (
     <div className='profile'>
       <div className='profile__imageContainer'>
@@ -12,14 +16,20 @@ export default function YourProfile({state, setState}) {
           cloudName='dnggclzfd'
           publicId={state.user.image}
         />
-        <i class="fa-solid fa-pen-to-square profile__imageIcon"></i>
+        <i 
+          class="fa-solid fa-pen-to-square profile__imageIcon"  
+          onClick={() => setChangeImage(true)}
+        ></i>
       </div>
 
       <h5 className='profile__title'>name:</h5>
       <div className='profile__nameContainer'>
         <i class="fa-solid fa-pen-to-square profile__icon hide"></i>
         <h2 className='profile__info'>{state.user.name}</h2>
-        <i class="fa-solid fa-pen-to-square profile__icon"></i>
+        <i
+          class="fa-solid fa-pen-to-square profile__icon"
+          onClick={() => setChangeName(true)}
+        ></i>
       </div>
       
       <h5 className='profile__title'>email:</h5>
@@ -29,7 +39,10 @@ export default function YourProfile({state, setState}) {
       <div className='profile__bioContainer'>
         <i class="fa-solid fa-pen-to-square profile__icon hide"></i>
         <h2 className='profile__info'>{state.user.bio}</h2>
-        <i class="fa-solid fa-pen-to-square profile__icon"></i>
+        <i
+          class="fa-solid fa-pen-to-square profile__icon"
+          onClick={() => setChangeBio(true)}
+        ></i>
       </div>
     </div>
   )
