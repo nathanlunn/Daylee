6062de1e605a17d707e3a875de7d3369e2088de6
Summer <nathandouglaslunn@gmail.com>
2022-11-29 06:31:50 -0500

    reorganize name container to display content in the right order

--- client/src/components/YourProfile.js
+++ client/src/components/YourProfile.js
@@ -8,6 +8,8 @@ export default function YourProfile({state, setState}) {
   const [changeName, setChangeName] = useState(false);
   const [changeBio, setChangeBio] = useState(false);
   const [imageURL, setImageURL] = useState('');
+  const [name, setName] = useState(state.user.name);
+  const [bio, setBio] = useState(state.user.bio);
 
   const uploadImage = (image) => {
     const formData = new FormData();
@@ -101,16 +103,46 @@ export default function YourProfile({state, setState}) {
           </>
         )}
       </div>
+      
+      {changeName ? (
+        <div>
+          <h5 className='profile__title'>name:</h5>
+          <div className='profile__nameContainer'>
+            <i class="fa-solid fa-pen-to-square profile__icon hide"></i>
+            <h2 className='profile__info'>{state.user.name}</h2>
+            <div className='profile__buttonContainer'>
+                <button
+                  className='profile__button profile__button--cancel'
+                  onClick={() => {
+                    setName(state.user.name);
+                    setChangeName(false);
+                  }}
+                >cancel</button>
+                <button
+                  className='profile__button profile__button--confirm'
+                  onClick={() => {changeProfile('name', name)}}
+                >confirm</button>
+              </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className='profile__nameContainer'>
+            <h5 className='profile__title'>name:</h5>
 
-      <h5 className='profile__title'>name:</h5>
-      <div className='profile__nameContainer'>
-        <i class="fa-solid fa-pen-to-square profile__icon hide"></i>
-        <h2 className='profile__info'>{state.user.name}</h2>
-        <i
-          class="fa-solid fa-pen-to-square profile__icon"
-          onClick={() => setChangeName(true)}
-        ></i>
-      </div>
+            <div className='profile__nameContentContainer'>
+              <i class="fa-solid fa-pen-to-square profile__icon hide"></i>
+
+              <h2 className='profile__info'>{state.user.name}</h2>
+              
+              <i
+                class="fa-solid fa-pen-to-square profile__icon"
+                onClick={() => setChangeName(true)}
+              ></i>
+            </div>
+          </div>
+        </div>
+      )}
       
       <h5 className='profile__title'>email:</h5>
       <h2 className='profile__info'>{state.user.email}</h2>
