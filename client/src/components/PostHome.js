0d5add508e3bd537fd9b6263f0e95effbde67095
Summer <nathandouglaslunn@gmail.com>
2022-12-04 05:27:46 -0500

    render the comment component for each comment that is associated with today's topic

--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Image} from 'cloudinary-react';
 import '../styles/PostHome.css';
+import Comment from './Comment.js';
 
 export default function PostHome({state, setState}) {
   const [comments, setComments] = useState([]);
@@ -17,6 +18,16 @@ export default function PostHome({state, setState}) {
       })
   }, [state.topic])
 
+  const commentList = comments.map(comment => {
+    return (
+      <Comment
+        key={comment.id}
+        userID={comment.user_id}
+        content={comment.content}
+      />
+    )
+  })
+
   return (
     <div className='topic'>
       <Image 
@@ -24,7 +35,12 @@ export default function PostHome({state, setState}) {
         cloudName='dnggclzfd'
         publicId={state.topic.image}
       />
+      
       <h2 className='topic__title'>{state.topic.title}</h2>
+
+      <div className='topic__commentsList'>
+        {commentList}
+      </div>
     </div>
   )
 }
