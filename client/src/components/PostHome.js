bef11e07c3ca82f9e6e4d5bcb74987a5bd4ed23c
Summer <nathandouglaslunn@gmail.com>
2022-12-04 06:05:29 -0500

    create new comment box to type in a comment for the Daylee post and add a character counter

--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -6,6 +6,8 @@ import Comment from './Comment.js';
 
 export default function PostHome({state, setState}) {
   const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const [commentCharacterCount, setCommentCharacterCount] = useState(0);
 
   useEffect(() => {
     axios.get(`http://localhost:8000/topics/comments/${state.topic.id}`)
@@ -37,6 +39,26 @@ export default function PostHome({state, setState}) {
       />
       
       <h2 className='topic__title'>{state.topic.title}</h2>
+
+      <div className='topic__newCommentContainer'>
+        <h3 className='topic__callToComment'>What Are Your Thoughts?</h3>
+
+        <textarea 
+          className='topic__newCommentInput'
+          value={newComment}
+          onChange={e => {
+            setNewComment(e.target.value);
+            setCommentCharacterCount(e.target.value.length)
+          }}
+
+        ></textarea>
+
+        <div className='topic__newCommentBottom'>
+        <h3 
+          className={commentCharacterCount > 100 ? 'topic__commentCharacterCount red' :'topic__commentCharacterCount'}
+        >{`${commentCharacterCount} / 100`}</h3>
+        </div>
+      </div>
 
       <div className='topic__commentsList'>
         {commentList}
